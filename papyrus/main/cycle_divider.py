ea30c790d5ae2021ce793cae1500009ca5f27896
taekwan-hwang <xorhks0005@gmail.com>
2018-01-16 19:18:40 +0900

    modify logic in computing p-variance

--- papyrus/main/cycle_divider.py
+++ papyrus/main/cycle_divider.py
@@ -89,7 +89,7 @@ def mean_pain_variance_by_cycle(cycle):
                 continue
         var=numpy.var(pains)*len(pains)/(len(pains)-1)
         if numpy.isnan(var):
-            variances.append(0)
+            pass
         else:
             variances.append(var)
     return numpy.mean(variances)
