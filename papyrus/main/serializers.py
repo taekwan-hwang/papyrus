b96aa05330bc7e959e2417855462e8e5d86959f3
taekwan-hwang <xorhks0005@gmail.com>
2018-01-20 16:15:30 +0900

    remove unnecessary code add folder for statistics

--- papyrus/main/serializers.py
+++ papyrus/main/serializers.py
@@ -1,21 +1,6 @@
 from rest_framework import serializers
 from main.models import Test, Time, Tong2, Tong5, Tong8
 #from main.models import tong6
-"""class tong6Serializer(serializers.ModelSerializer):
-    class Meta:
-        #model=tong6
-        fields = ('reg_num', 'date', 'datetime', 'look_code','look_codeNM', 'source', 'numeric')
-"""
-
-class TestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Test
-        fields = '__all__'
-
-class TimeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Time
-        fields='__all__'
 
 class Tong2Serializer(serializers.ModelSerializer):
     class Meta:
--- papyrus/main/views.py
+++ papyrus/main/views.py
@@ -5,38 +5,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.renderers import JSONRenderer
-from main.serializers import TestSerializer, TimeSerializer, Tong2Serializer, Tong5Serializer, Tong8Serializer
-from main.cycle_divider import get_by_cycle, get_by_person, mean_pain_variance_by_cycle
+from main.serializers import Tong2Serializer, Tong5Serializer, Tong8Serializer
 from main.models import Tong2, Tong5, Tong8
-class TestGetByCycle(APIView):
-    def get(self, request, format=None):
-        serializer=TestSerializer(get_by_cycle(1), many=True)
-        return Response(serializer.data)
-    """
-    List all snippets, or create a new snippet.
-    """
-    """
-    def get(self, request, format=None):
-        query_set=tong6.objects.all()
-        serializer = tong6Serializer(query_set, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request, format=None):
-        name=str(request.data.get('name', 'taekwan'))
-        num=str(request.data.get('num', '123'))
-        query_set=tong6.objects.filter(reg_num=499324)
-        serializer = tong6Serializer(query_set, many=True)
-        return Response(name+num)
-    """
-
-class TestGetByPerson(APIView):
-    def get(self, request, format=None):
-        serializer=TimeSerializer(get_by_person(3), many=True)
-        return Response(serializer.data)
-
-class TestVarianceMean(APIView):
-    def get(self, request, format=None):
-        return Response(mean_pain_variance_by_cycle(3))
 
 class Tong2View(APIView):
     def get(self, request, format=None, pi=0):
